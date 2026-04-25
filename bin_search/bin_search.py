7ce7d0730863f22adcdfc147f9fec5a1ab179383
unknown <iq000012@gmail.com>
2026-04-25 23:54:37 +0500

    bad code

--- bin_search/bin_search.py
+++ bin_search/bin_search.py
@@ -3,9 +3,30 @@ from __future__ import annotations
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        idx = -1
+        if nums[0] == target:
+            return 0
+        if nums[len(nums) - 1] == target:
+            return len(nums) - 1
+
+        left = 1
+        right = len(nums) - 1
+
+        min_idx = 0
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] <= nums[min_idx]:
+                min_idx = mid
+                right = mid - 1
+            elif nums[mid] > nums[min_idx]:
+                left = mid + 1
+
         left = 0
         right = len(nums) - 1
+        if min_idx > left and target >= nums[left]:
+            right = min_idx
+        else:
+            left = min_idx
+
         while left <= right:
             mid = (right + left) // 2
             if nums[mid] < target:
@@ -13,10 +34,10 @@ class Solution:
             elif nums[mid] > target:
                 right = mid - 1
             else:
-                idx = mid
-                break
+                return mid
 
-        return idx
+        return -1
 
-
-print(Solution().search([-1,0,2,4,6,8], 4))
+print(Solution().search([3,4,5,6,1,2], 1))  # 4
+print(Solution().search([-1,0,2,4,6,8], 4))  # 3
+print(Solution().search([3,1], 3))  # 0
---
+++ bin_search/search_rotated_sorted.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            while
+            mid = (left + right) // 2
+            if nums[mid] != target:
+                if
+            else:
+                return mid
+
+        return -1
+
+# print(Solution().search([3,4,5,6,1,2], 1))  # 4
+# print(Solution().search([3,5,6,0,1,2], 4))  # -1
+# print(Solution().search([1,3], 3))  # 1
+print(Solution().search([5,1,2,3,4], 1))  # 1
