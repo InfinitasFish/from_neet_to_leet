0a7b943ef1653a8e0c404b496e1c7cfef13d02d5
unknown <iq000012@gmail.com>
2026-04-26 18:12:10 +0500

    refactor

--- bin_search/find_min_rotated.py
+++ bin_search/find_min_rotated.py
@@ -3,21 +3,26 @@ from __future__ import annotations
 
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        left = 1
+        left = 0
         right = len(nums) - 1
         min_cur = nums[0]
         while left <= right:
+            if nums[left] < nums[right]:
+                min_cur = min(min_cur, nums[left])
+                break
+
             mid = (left + right) // 2
-            if nums[mid] <= min_cur:
-                min_cur = nums[mid]
-                right = mid - 1
-            elif nums[mid] > min_cur:
+            min_cur = min(min_cur, nums[mid])
+            if nums[mid] >= nums[left]:
                 left = mid + 1
+            else:
+                right = mid - 1
 
         return min_cur
 
-#
-# print(Solution().findMin([4,5,0,1,2,3]))
-# print(Solution().findMin([3,4,5,6,1,2]))
-# print(Solution().findMin([4,5,6,7]))
+
+print(Solution().findMin([4,5,0,1,2,3]))
+print(Solution().findMin([3,4,5,6,1,2]))
+print(Solution().findMin([4,5,6,7]))
 print(Solution().findMin([2,1]))
+print(Solution().findMin([5,1,2,3,4]))
--- bin_search/search_rotated_sorted.py
+++ bin_search/search_rotated_sorted.py
@@ -5,18 +5,38 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left = 0
         right = len(nums) - 1
-
+        min_idx = 0
         while left <= right:
-            while
+            if nums[left] < nums[right]:
+                min_idx = left if nums[left] < nums[min_idx] else min_idx
+                break
+
             mid = (left + right) // 2
-            if nums[mid] != target:
-                if
+            min_idx = mid if nums[mid] < nums[min_idx] else min_idx
+            if nums[mid] >= nums[left]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        left = 0
+        right = len(nums) - 1
+        if nums[min_idx] <= target <= nums[right]:
+            left = min_idx
+        else:
+            right = min_idx - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
             else:
                 return mid
 
         return -1
-
+#
 # print(Solution().search([3,4,5,6,1,2], 1))  # 4
 # print(Solution().search([3,5,6,0,1,2], 4))  # -1
 # print(Solution().search([1,3], 3))  # 1
-print(Solution().search([5,1,2,3,4], 1))  # 1
+# print(Solution().search([5,1,2,3,4], 1))  # 1
+print(Solution().search([4,5,6,7,0,1,2], 0))  # 4
